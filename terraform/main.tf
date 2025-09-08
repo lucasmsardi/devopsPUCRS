86d9bf9eeb2f7c56be9b3980d6a6e1bddac94b5a
lucasmsardi <lucasmsardi@gmail.com>
2025-09-08 20:41:00 -0300

    feat: exposing port 3000 hopefully

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,3 @@
-
 terraform {
   required_version = ">= 1.5.0"
   required_providers {
@@ -13,9 +12,29 @@ provider "aws" {
   region = "sa-east-1"
 }
 
+resource "aws_security_group" "app_sg" {
+  name        = "devops-project"
+  description = "Allow access to app"
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "app" {
-  ami           = "ami-02556f6726aa38019" 
-  instance_type = "t2.micro"
+  ami                    = "ami-02556f6726aa38019"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.app_sg.id]
 
   user_data = <<-EOF
     #!/bin/bash
@@ -25,5 +44,14 @@ resource "aws_instance" "app" {
     usermod -a -G docker ec2-user
     docker run -d -p 3000:3000 sha256:a20dfd6946b40880b22b00b748a2aa0608998d7b6492f4052307163b9ecc92cd
   EOF
+}
 
+output "instance_id" {
+  description = "ID of the EC2 instance"
+  value       = aws_instance.app.id
+}
+
+output "public_ip" {
+  description = "Public IP of the EC2 instance"
+  value       = aws_instance.app.public_ip
 }
